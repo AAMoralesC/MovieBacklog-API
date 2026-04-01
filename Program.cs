17f92b02aa7ecdeee1e1362e61aff541d3185604
AAMoralesC <109701155+AAMoralesC@users.noreply.github.com>
2026-03-31 23:40:39 -0300

    primer crud en .NET 10

---
+++ Program.cs
@@ -0,0 +1,70 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Configurar serivicios (SQLite)
+builder.Services.AddDbContext<MovieDb>(opt =>opt.UseSqlite("Data Source=movies.db"));
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configuracion del servidor
+ if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+app.UseHttpsRedirection();
+
+//--Endpoint (CRUD)--
+
+// 1. Obtener todas las películas (READ)
+app.MapGet("/movies", async (MovieDb db) => await db.Movies.ToListAsync());
+
+// 2. Obtener peli por ID (Read)
+app.MapGet("/movies/{id}", async (int id, MovieDb db) =>await db.Movies.FindAsync(id) is Movie m ? Results.Ok(m) : Results.NotFound());
+
+// 3. Crear una película (Create)   
+app.MapPost("/movies", async (Movie movie, MovieDb db) =>{db.Movies.Add(movie);
+await db.SaveChangesAsync();
+return Results.Created($"/movies/{movie.Id}", movie);
+});
+
+// 4. Actualizar una película (Update)
+app.MapPut("/movies/{id}", async (int id, Movie inputmovie, MovieDb db) =>
+{
+    var movie = await db.Movies.FindAsync(id);
+    if (movie is null) return Results.NotFound();
+
+    movie.Title = inputmovie.Title;
+    movie.Watched = inputmovie.Watched;
+
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+// 5. Eliminar (Delete
+app.MapDelete("/movies/{id}", async (int id, MovieDb db) =>
+{
+    if (await db.Movies.FindAsync(id) is Movie movie)
+    {
+        db.Movies.Remove(movie);
+        await db.SaveChangesAsync();
+        return Results.NoContent();
+    }
+    return Results.NotFound();
+
+});
+
+app.Run();
+
+// Clases
+class Movie {
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public bool Watched { get; set; }
+}
+
+class MovieDb : DbContext {
+    public MovieDb(DbContextOptions<MovieDb> options) : base(options) { }
+    public DbSet<Movie> Movies => Set<Movie>();
+}
